321a57edfb8ca573f7e696defcbfc343f55c4fab
qilei123 <qileimail123@gmail.com>
2018-09-07 16:09:45 -0400

    fsdafd

--- lib/rpn/proposal_target_layer.py
+++ lib/rpn/proposal_target_layer.py
@@ -124,8 +124,8 @@ def _get_bbox_regression_labels(bbox_target_data, num_classes):
         cls = clss[ind]
         start = 4 * cls
         end = start + 4
-        bbox_targets[ind, start:end] = bbox_target_data[ind, 1:]
-        bbox_inside_weights[ind, start:end] = cfg.TRAIN.BBOX_INSIDE_WEIGHTS
+        bbox_targets[ind, int(start):int(end)] = bbox_target_data[ind, 1:]
+        bbox_inside_weights[ind, int(start):int(end)] = cfg.TRAIN.BBOX_INSIDE_WEIGHTS
     return bbox_targets, bbox_inside_weights
 
 
